726e0a58184b0125d5cb273ea034e2b716b014cf
Guilherme Carvalho <guilhermecarvalhodev693@gmail.com>
2024-04-08 22:20:30 -0300

    Initial commit

---
+++ script/index.js
@@ -0,0 +1,19 @@
+
+
+class Produto {
+    constructor() {
+        this.id = 0
+        this.nomeProduto = ''   
+        this.valor = 0
+    }
+
+    adicionar() {
+        alert('adicionou')
+    }
+
+    excluir() {
+        alert('item excluido')
+    }
+}
+
+var produto = new Produto()
