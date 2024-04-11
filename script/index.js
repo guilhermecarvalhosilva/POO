2c3576de63327de46dbde9368fea0445eea78f53
Guilherme Carvalho <guilhermecarvalhodev693@gmail.com>
2024-04-10 21:44:33 -0300

    atualização

--- script/index.js
+++ script/index.js
@@ -1,10 +1,35 @@
 class Produto {
     constructor() {
-        this.dados = []
+        this.dados = 0
+        this.listaProduto = []
     }
 
     salvar() {
+         let produto = this.lerDados()
+         this.validacao(produto)
+
+         console.log(produto)
+    }
+
+    lerDados() {
+        let produto = {}
+
+        produto.nome = document.getElementById('txtproduto').value,
+        produto.preco = document.getElementById('txtpreco').value
         
+        return produto
+       
+    }
+
+    validacao(produto) {
+        let mensagem = ""
+        if(produto.nome.length > 0) {
+            mensagem = 'acertou'
+        } else {
+            mensagem = 'erro'
+        }
+
+        alert(mensagem)
     }
 
     cancelar() {
